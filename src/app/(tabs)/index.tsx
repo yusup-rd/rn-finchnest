e81da81787cef5101b8789c13c6ffa50ff649e6a
Yusup <yusuprd@icloud.com>
2026-09-11 19:33:09 +0500

    feat: Add custom fonts

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,6 @@
 import { Link } from "expo-router";
 import { styled } from "nativewind";
-import { Text } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
 
 const SafeAreaView = styled(RNSafeAreaView);
@@ -8,40 +8,28 @@ const SafeAreaView = styled(RNSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className="bg-background flex-1 p-5">
-      <Text className="text-success text-xl font-bold">
-        Welcome to Nativewind!
-      </Text>
-      <Link href="/onboarding" className="bg-primary rounded p-4 text-white">
-        Go to Onboarding
-      </Link>
-      <Link
-        href="/(auth)/sign-in"
-        className="bg-primary rounded p-4 text-white"
-      >
-        Go to Sign In
-      </Link>
-      <Link
-        href="/(auth)/sign-up"
-        className="bg-primary rounded p-4 text-white"
-      >
-        Go to Sign Up
-      </Link>
+      <Text className="font-sans-extrabold text-5xl">Home</Text>
 
-      <Link
-        href="./subscriptions/spotify"
-        className="bg-primary rounded p-4 text-white"
-      >
-        Spotify Subscription
-      </Link>
-      <Link
-        href={{
-          pathname: "./subscriptions/claude-max",
-          params: { id: "claude" },
-        }}
-        className="bg-primary rounded p-4 text-white"
-      >
-        Claude Max Subscription
-      </Link>
+      <View className="mt-4 gap-4">
+        <Link
+          href="/onboarding"
+          className="bg-primary font-sans-bold rounded p-4 text-white"
+        >
+          Go to Onboarding
+        </Link>
+        <Link
+          href="/(auth)/sign-in"
+          className="bg-primary font-sans-bold rounded p-4 text-white"
+        >
+          Go to Sign In
+        </Link>
+        <Link
+          href="/(auth)/sign-up"
+          className="bg-primary font-sans-bold rounded p-4 text-white"
+        >
+          Go to Sign Up
+        </Link>
+      </View>
     </SafeAreaView>
   );
 }
--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,6 +1,27 @@
 import "@root/global.css";
-import { Stack } from "expo-router";
+import { useFonts } from "expo-font";
+import { SplashScreen, Stack } from "expo-router";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    "sans-regular": require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
+    "sans-medium": require("@/assets/fonts/PlusJakartaSans-Medium.ttf"),
+    "sans-semibold": require("@/assets/fonts/PlusJakartaSans-SemiBold.ttf"),
+    "sans-bold": require("@/assets/fonts/PlusJakartaSans-Bold.ttf"),
+    "sans-light": require("@/assets/fonts/PlusJakartaSans-Light.ttf"),
+    "sans-extrabold": require("@/assets/fonts/PlusJakartaSans-ExtraBold.ttf"),
+  });
+
+  useEffect(() => {
+    if (fontsLoaded) {
+      SplashScreen.hideAsync();
+    }
+  }, [fontsLoaded]);
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
   return <Stack screenOptions={{ headerShown: false }} />;
 }
