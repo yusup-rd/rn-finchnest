0e78476b1847608aabc0c74be0e1732860796ada
Yusup <yusuprd@icloud.com>
2026-09-11 22:43:37 +0500

    fix: add preventAutoHideAsync

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -3,6 +3,8 @@ import { useFonts } from "expo-font";
 import { SplashScreen, Stack } from "expo-router";
 import { useEffect } from "react";
 
+SplashScreen.preventAutoHideAsync();
+
 export default function RootLayout() {
   const [fontsLoaded, fontError] = useFonts({
     "sans-regular": require("@/assets/fonts/PlusJakartaSans-Regular.ttf"),
