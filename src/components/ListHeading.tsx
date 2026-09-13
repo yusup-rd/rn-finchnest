c72a2813b320697787cc9b8c72ce1141241aff08
Yusup <yusuprd@icloud.com>
2026-09-13 19:15:00 +0500

    feat: add navigation on View All click

--- src/components/ListHeading.tsx
+++ src/components/ListHeading.tsx
@@ -1,10 +1,16 @@
+import { useRouter } from "expo-router";
 import { Text, TouchableOpacity, View } from "react-native";
 
 const ListHeading = ({ title }: ListHeadingProps) => {
+  const router = useRouter();
+  const handlePress = () => {
+    router.navigate("/(tabs)/subscriptions");
+  };
+
   return (
     <View className="list-head">
       <Text className="list-title">{title}</Text>
-      <TouchableOpacity className="list-action">
+      <TouchableOpacity className="list-action" onPress={handlePress}>
         <Text className="list-action-text">View All</Text>
       </TouchableOpacity>
     </View>
